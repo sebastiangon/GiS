51fd89329c6c452f69a7c3b549b36c8477ff981c
Sebastian Gonzalez <sebastiangonzalez@Sebastians-MacBook-Pro.local>
2018-01-21 19:54:34 -0300

    losing bt signal logic begin

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -3,38 +3,54 @@
 SoftwareSerial BT(4,2);      // RX, TX recordar que se cruzan
 
 int enginePin = 8;
-long lastShownTime = 0;
+
+long lastTickMss = 0;
 long elapsedTime = 0;
-boolean BLUETOOTH_ALIVE = false;
+
+boolean bluetoothAlive = false;
+boolean fisrtConnectionWasStablished = true;
+int lastConnectionMss = 0;
+
 
 void setup() { 
   Serial.begin(9600);
-  Serial.println("Enter AT commands now:");
+//  Serial.println("Enter AT commands now:");
   BT.begin(9600);
   pinMode(enginePin, OUTPUT);
+  digitalWrite(enginePin, HIGH);
 }
 
 void loop() {
+  
   delay(1500);
   
-  BLUETOOTH_ALIVE = false;
+  bluetoothAlive = false;
+  
   while (BT.available()) {
      Serial.write(BT.read());
-     BLUETOOTH_ALIVE = true;
+     bluetoothAlive = true;
+     fisrtConnectionWasStablished = true;
      digitalWrite(enginePin, HIGH);
   }
   
-  if (BLUETOOTH_ALIVE) { BT.write("pong"); Serial.write("\nsent pong\n"); }
-
-  showElapsedTime();
-
+  if (bluetoothAlive) {
+    BT.write("pong"); Serial.write("\npong sent\n");
+  }
+  
+  executeTimedRutines();
+  
  }
 
-String showElapsedTime() {
+String executeTimedRutines() {
+  
   elapsedTime = millis();
   
-  if ( (elapsedTime - lastShownTime) >= 10000 ) { // Every 10 seconds at least
+  if ( (elapsedTime - lastTickMss) >= 10000 ) { // Every 10 seconds
     Serial.println(elapsedTime / 1000);
-    lastShownTime = elapsedTime;
+    lastTickMss = elapsedTime;
+
+    if ( !bluetoothAlive && fisrtConnectionWasStablished ) {
+      // retryConnection ???? conter++, cada 10 segundos.... si despues de 100 segundos no paso nada, apagamos motor
+    }
   }
 }
