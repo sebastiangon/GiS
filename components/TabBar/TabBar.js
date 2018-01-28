02e6bf485d03511097c42f479f7a931bd2a00ad3
Sebastian Gonzalez <sebastiangonzalez@Sebastians-MacBook-Pro.local>
2018-01-28 11:10:05 -0300

    create tabBar component

---
+++ components/TabBar/TabBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
+
+import styles from './Styles';
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
